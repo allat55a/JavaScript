82de283679c81225b73d34b08f595f0a52e815b1
allat55a <allat55a@gmail.com>
2022-06-22 15:05:17 +0300

    lesson

---
+++ l_1_30.js
@@ -0,0 +1,36 @@
+let user_name = "Alla"
+let name2 = "Alla"
+
+// console.log(name2)
+
+let age = 24
+// console.log(age)
+
+// let person = user_name + " "+ age 
+// console.log(user_name + " "+ age)
+let user_age = 30
+let work_period =10
+
+let total_time = user_age / work_period
+// console.log("total_time = ",  total_time)
+
+let morning = false
+console.log ("morning = ", morning, typeof(morning ))
+
+let evening = true
+console.log ("evening = ", evening, typeof(evening))
+
+let apples = 15
+let cherry = 5
+
+let compare = apples >= cherry
+console.log("compare =", compare)
+if (compare){
+    console.log("Compare is", compare)
+}else if (apples == 15) {
+    console.log ("apples ==", apples)
+
+}
+ else {
+    console.log("!!_Compare is", false)
+}
